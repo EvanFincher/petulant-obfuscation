825cf72bc7ab7b5dd28cefe85f8fe2fc86c9747d
fleetofthemalden <dewey.moor206@gmail.com>
2014-11-11 11:39:26 -0500

    first iteration of server:join

--- server.erl
+++ server.erl
@@ -1,24 +1,33 @@
 -module(server).
--export([start/1,test/1]).
+-export([start/1]).
 
+-record(player, {name, pid}).
+-record(tile, {space, playerName}).
 
 %starts the server and registers it under the name provided
 
 start(SName) ->
   register(SName, self()),
-  loop([]).
+  loop(maps:new(), maps:new()).
 
 
-loop(PlayerList) ->
+loop(Players, Board) ->
   receive
+    {join, Sender} ->
+      NewPlayers = join(Sender, Players),
+      Sender ! {ok, NewPlayers},
+      loop(NewPlayers, Board);
     {ping, Sender} ->
       Sender ! {ok, self()},
-      loop(PlayerList);
+      loop(Players, Board);
     {stop, Sender} ->
       Sender ! {stopping, self()},
       ok
   end.
 
+join(NewPlayerPid, Players) ->
+   PlayerName = maps:size(Players),
+   NewPlayer = #player{name=PlayerName, pid=NewPlayerPid},
+   maps:put(PlayerName, NewPlayer, Players).
 
-test(Server) ->
-  ok.
+
