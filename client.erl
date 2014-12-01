46c7792290e2b286f50650b520a3f3ee301906a3
Evan Fincher <evan@finchers.net>
2014-11-30 16:15:01 -0800

    Added a populate client function that when passed (preset, one) drops some rocks and killable but fake players onto the board.  Also added support for killing other players, triggered by client call act(SName,Node,{attack,Direction},PlayerName).

--- client.erl
+++ client.erl
@@ -1,5 +1,5 @@
 -module(client).
--export([start/2,stop/2,restart/2,join/2,lookup/3,ping/2,refresh/2,act/4,test/2]).
+-export([start/2,stop/2,restart/2,join/2,populate/4,lookup/3,ping/2,refresh/2,act/4]).
 
 
 %spawns a new server registered under SName; pings to verify success
@@ -30,6 +30,14 @@ join(SName,Node) ->
     {joined, PlayerName, Board} -> {PlayerName, Board}
   end.
 
+%signals the server to add stuff to the board
+%e.g. (game,Node,preset,one)
+populate(SName, Node, Type, Data) ->
+  {SName, Node} ! {populate, Type, Data, self()},
+  receive
+    {populated, Board} -> Board
+  end.
+
 %fetch information for player 'PlayerName'
 lookup(SName, Node, PlayerName) ->
   {SName, Node} ! {lookup, PlayerName, self()},
@@ -58,7 +66,8 @@ refresh(SName, Node) ->
   end. 
 
 %send an action to the server for processing
-%e.g. {move, north}; PlayerName was returned by join function 
+%e.g. {move, north}, or {attack, east}
+%PlayerName was returned by join function 
 act(SName, Node, Action, PlayerName) ->
   {SName, Node} ! {action, Action, PlayerName, self()},
   receive
@@ -66,10 +75,10 @@ act(SName, Node, Action, PlayerName) ->
     {failed, Board} -> {failed, Board}
   end.
 
-test(SName, Node) ->
-  start(SName, Node),
-  {PlayerName, _Board} = join(SName, Node),
-  Board1 = refresh(SName, Node),
-  {ok, Board2} = act(SName, Node, {move, north}, PlayerName),
-  L1 = [{refresh, ok}, {act, ok}, Board1].
+%test(SName, Node) ->
+%  start(SName, Node),
+%  {PlayerName, _Board} = join(SName, Node),
+%  Board1 = refresh(SName, Node),
+%  {ok, Board2} = act(SName, Node, {move, north}, PlayerName),
+%  L1 = [{refresh, ok}, {act, ok}, Board1].
   
